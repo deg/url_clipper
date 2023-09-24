c80ba37d2d2cab0cf61c37e14a10c4cd9c7b32fe
David Goldfarb <deg@degel.com>
2023-09-24 15:24:46 +0300

    Move to styled-components and other cleanups

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+import styled, {createGlobalStyle} from 'styled-components';
 
 function App() {
   const [cleanedUrl, setCleanedUrl] = useState(null);
@@ -12,58 +12,121 @@ function App() {
   }, []);
 
   return (
-    <div style={{ maxWidth: "60ch", margin: "auto" }}>
-      <div id="cleanedUrl"
-           style={{ margin: "10px auto", textAlign: "center", wordWrap: "break-word" }}>
-        {cleanedUrl}
-      </div>
-      <div style={{ textAlign: "center" }}>
-        <button id="copyUrlBtn" onClick={()=>copyFilteredUrlToClipboard(cleanedUrl)}>
-          <img id="icon" src="copy_url_icon_128.png" alt="Copy URL"/>
-          Copy URL to Clipboard
-        </button>
-      </div>
-    </div>
+    <>
+      <GlobalStyle/>
+      <Container>
+        <UrlContainer id="cleanedUrl">
+          {cleanedUrl}
+        </UrlContainer>
+        <CenteredDiv>
+          <CopyButton id="copyUrlBtn" onClick={() => copyUrlThenClosePopup(cleanedUrl)}>
+            <img id="icon" src="copy_url_icon_128.png" alt="Copy URL"/>
+            Copy URL to Clipboard
+          </CopyButton>
+        </CenteredDiv>
+      </Container>
+    </>
   );
 }
 
 const initializeExtension = async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  const url = tab.url;
-  let cleanedUrl = removeChromeExtensionWrapper(url);
-  cleanedUrl = removeTrackingParams(url);
-  return cleanedUrl;
+  return cleanUrl(tab.url);
 };
 
-const copyFilteredUrlToClipboard = (cleanedUrl) => {
-  navigator.clipboard.writeText(cleanedUrl).then(
+
+const cleanUrl = (url) => {
+    // Remove any Adobe's PDF viewer wrapper
+  const removeChromeExtensionWrapper = (url) => {
+    const adobePrefix = "chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/";
+    if (url.startsWith(adobePrefix)) {
+      return url.replace(adobePrefix, "");
+    }
+    return url;
+  };
+
+  // Remove tracking parameters from the URL
+  const TRACKING_PARAMS = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
+  const GOOGLE_DOCS_PARAMS = ['pli'];
+  const NOISE_PARAMS = [...TRACKING_PARAMS, ...GOOGLE_DOCS_PARAMS];
+  const removeTrackingParams = (url) => {
+    const parsedUrl = new URL(url);
+    const params = parsedUrl.searchParams;
+    NOISE_PARAMS.forEach((param) => params.delete(param));
+    return parsedUrl.toString();
+  };
+
+  let cleaned = url;
+  cleaned = removeChromeExtensionWrapper(cleaned);
+  cleaned = removeTrackingParams(url);
+  return cleaned;
+}
+
+
+const copyUrlThenClosePopup = (url) => {
+  navigator.clipboard.writeText(url).then(
     () => window.close(),
     (err) => console.error("Failed to copy URL: ", err)
   );
 };
 
 
-// Remove any Adobe's PDF viewer wrapper
-const removeChromeExtensionWrapper = (url) => {
-  if (url.startsWith("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/")) {
-    return url.replace("chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/", "");
+const GlobalStyle = createGlobalStyle`
+  :root {
+    font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+    line-height: 1.5;
+    font-weight: 400;
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-text-size-adjust: 100%;
   }
-  return url;
-};
 
+  body {
+    margin: 0;
+    display: flex;
+    place-items: center;
+  }
+`;
 
-// Remove tracking parameters from the URL
-const TRACKING_PARAMS = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content'];
-const GOOGLE_DOCS_PARAMS = ['pli'];
-const NOISE_PARAMS = [...TRACKING_PARAMS, ...GOOGLE_DOCS_PARAMS];
-const removeTrackingParams = (url) => {
-  const parsedUrl = new URL(url);
-  const params = parsedUrl.searchParams;
-  NOISE_PARAMS.forEach((param) => params.delete(param));
-  return parsedUrl.toString();
-};
-// Initialize the extension when the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", initializeExtension);
+const Container = styled.div`
+  padding: 2rem;
+  text-align: center;
+  max-width: 60ch;
+  margin: 0 auto;
+`;
+const UrlContainer = styled.div`
+  margin: 10px auto;
+  text-align: center;
+  word-wrap: break-word;
+`;
+
+const CenteredDiv = styled.div`
+  text-align: center;
+`;
+
+const CopyButton = styled.button`
+  border-radius: 8px;
+  border: 1px solid transparent;
+  padding: 0.6em 1.2em;
+  font-size: 1em;
+  font-weight: 500;
+  font-family: inherit;
+  background-color: #f0f0f0;
+  cursor: pointer;
+  transition: border-color 0.25s;
+
+  &:hover {
+    border-color: #646cff;
+  background-color: #f9f9f9;
+  }
+
+  &:focus,
+  &:focus-visible {
+    outline: 4px auto -webkit-focus-ring-color;
+  }
+`;
 
 
 export default App;
